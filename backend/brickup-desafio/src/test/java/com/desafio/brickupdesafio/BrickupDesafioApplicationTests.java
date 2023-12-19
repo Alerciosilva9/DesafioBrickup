a8890d3dd54a806758ca1226f979f1effcf493ea
alerciosilva9 <silvaalercio@gmail.com>
2023-12-19 14:52:40 -0300

    Criando estrtura de pastas

---
+++ backend/brickup-desafio/src/test/java/com/desafio/brickupdesafio/BrickupDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.brickupdesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrickupDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
